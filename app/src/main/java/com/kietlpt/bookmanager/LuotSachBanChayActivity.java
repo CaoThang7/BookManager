936db5e9f64a47787dba13d8a1552b3256ac0cad
CaoThang <benydeep123@gmail.com>
2021-06-15 22:07:43 +0700

    Ứng dụng quản lí sách

---
+++ app/src/main/java/com/kietlpt/bookmanager/LuotSachBanChayActivity.java
@@ -0,0 +1,44 @@
+package com.kietlpt.bookmanager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kietlpt.bookmanager.adapter.BookAdapter;
+import com.kietlpt.bookmanager.dao.SachDAO;
+import com.kietlpt.bookmanager.model.Sach;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LuotSachBanChayActivity extends AppCompatActivity {
+    public static List<Sach> dsSach = new ArrayList<>();
+    ListView lvBook;
+    BookAdapter adapter = null;
+    SachDAO sachDAO;
+    EditText edThang;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("TOP 10 SÁCH BÁN CHẠY");
+        setContentView(R.layout.activity_luot_sach_ban_chay);
+        lvBook = (ListView) findViewById(R.id.lvBookTop);
+        edThang = (EditText) findViewById(R.id.edThang);
+
+    }
+    public void VIEW_SACH_TOP_10(View view){
+        if (Integer.parseInt(edThang.getText().toString())>13 || Integer.parseInt(edThang.getText().toString())<0){
+            Toast.makeText(getApplicationContext(),"Không đúng định dạng tháng (1-12)",Toast.LENGTH_SHORT).show();
+        }else {
+            sachDAO = new SachDAO(LuotSachBanChayActivity.this);
+            dsSach = sachDAO.getSachTop10(edThang.getText().toString());
+
+            adapter = new BookAdapter(this, dsSach);
+            lvBook.setAdapter(adapter);
+        }
+    }
+}
